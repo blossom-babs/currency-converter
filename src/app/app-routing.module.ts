cfd8a4d764508ffa6044fefbcc8579a3cc8beae3
Blossom Babalola <sapashb@gmail.com>
2021-03-11 09:56:52 +0100

    style the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ConverterComponent } from './converter/converter.component';
+import { DummyComponent } from './dummy/dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ConverterComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'dummy', component: DummyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
