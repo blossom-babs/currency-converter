44a5cc6c7503144cb30ae4b58f62a1bc13199555
blossom-babs <saphashb@gmail.com>
2021-03-11 19:43:58 +0100

    write the logic to convert currencies

--- src/app/services/exchangerates.service.ts
+++ src/app/services/exchangerates.service.ts
@@ -1,13 +1,15 @@
+import { ExchangeRatesResponse } from './payload/exchange-rates-response';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExchangeratesService {
   constructor(private httpClient: HttpClient) {}
-  getRates(base: string) {
-    return this.httpClient.get(
+  getRates(base: string): Observable<ExchangeRatesResponse> {
+    return this.httpClient.get<ExchangeRatesResponse>(
       `https://api.exchangeratesapi.io/latest?base=${base}`
     );
   }
